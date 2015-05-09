1024eb28e9a64cdb10eee5f97d9bde849e439572
dessant <dessant@users.noreply.github.com>
2015-05-09 19:52:11 +0300

    fix py3 import error
    not tested with py2

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 __version__ = "0.1"
-from recycleview import RecycleView, RecycleLayoutManager, \
+from .recycleview import RecycleView, RecycleLayoutManager, \
     LinearRecycleLayoutManager, RecycleAdapter
