983dda627c154bf7f6d6a4fc81301650bed8aebe
Matthew Einhorn <moiein2000@gmail.com>
2015-07-12 17:49:57 -0400

    Introduces LayoutChangeException to stop layout early.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
 __version__ = "0.1"
 from .recycleview import RecycleView, RecycleLayoutManager, \
-    LinearRecycleLayoutManager, RecycleAdapter
+    LinearRecycleLayoutManager, RecycleAdapter, ViewBaseClass, \
+    LayoutChangeException
